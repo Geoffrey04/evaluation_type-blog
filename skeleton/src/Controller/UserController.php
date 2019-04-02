da50db24d3be1b2b0699070da05e99426be42f7e
Geoffrey04 <43751762+Geoffrey04@users.noreply.github.com>
2019-04-02 15:57:07 +0200

    commit middle

---
+++ skeleton/src/Controller/UserController.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\User;
+use App\Form\UserControllerType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+
+class UserController extends AbstractController
+{
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/inscription" , name="inscription")
+     *
+     */
+    public function inscription(Request $request)
+    {
+        $user = new User();
+
+
+        $form = $this->createForm(UserControllerType::class , $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $user->setRole("user");
+            $emptymanager = $this->getDoctrine()->getManager();
+            $emptymanager->persist($user);
+            $emptymanager->flush();
+
+          //  return $this->render("article/connexion.html.twig" , ["Form" => $form->createView()]);
+        }
+        return $this->render("article/connexion.html.twig" , ["form" => $form->createView()]);
+
+    }
+
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function connexion(Request $request, AuthenticationUtils $authenticationUtils)
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('article/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+
+}
+
+
+
---
+++ skeleton/src/Entity/Comments.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CommentsRepository")
+ */
+class Comments
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $contenu;
+
+    /**
+     * @ORM\Column(type="bigint")
+     */
+    private $id_com;
+
+    /**
+     * @ORM\Column(type="bigint")
+     */
+    private $id_user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): self
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getIdCom(): ?int
+    {
+        return $this->id_com;
+    }
+
+    public function setIdCom(int $id_com): self
+    {
+        $this->id_com = $id_com;
+
+        return $this;
+    }
+
+    public function getIdUser(): ?int
+    {
+        return $this->id_user;
+    }
+
+    public function setIdUser(int $id_user): self
+    {
+        $this->id_user = $id_user;
+
+        return $this;
+    }
+}
